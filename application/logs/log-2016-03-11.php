c93548c11ea836673265577d4991e8ec50a4bf8b
gaoxu <gao_xu@126.com>
2016-03-13 13:34:22 +0800

    aaa

---
+++ application/logs/log-2016-03-11.php
@@ -0,0 +1,44 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2016-03-11 02:32:31 --> Config Class Initialized
+INFO - 2016-03-11 02:32:31 --> Hooks Class Initialized
+DEBUG - 2016-03-11 02:32:31 --> UTF-8 Support Enabled
+INFO - 2016-03-11 02:32:31 --> Utf8 Class Initialized
+INFO - 2016-03-11 02:32:31 --> URI Class Initialized
+INFO - 2016-03-11 02:32:31 --> Router Class Initialized
+INFO - 2016-03-11 02:32:31 --> Output Class Initialized
+INFO - 2016-03-11 02:32:31 --> Security Class Initialized
+DEBUG - 2016-03-11 02:32:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-03-11 02:32:31 --> Input Class Initialized
+INFO - 2016-03-11 02:32:31 --> Language Class Initialized
+INFO - 2016-03-11 02:32:31 --> Loader Class Initialized
+INFO - 2016-03-11 02:32:31 --> Helper loaded: url_helper
+INFO - 2016-03-11 02:32:31 --> Session: Class initialized using 'files' driver.
+INFO - 2016-03-11 02:32:31 --> Controller Class Initialized
+INFO - 2016-03-11 02:32:31 --> Model Class Initialized
+INFO - 2016-03-11 02:32:31 --> Database Driver Class Initialized
+ERROR - 2016-03-11 02:32:33 --> Severity: Warning --> mysqli::real_connect():  D:\soft\xampp\htdocs\cloud_system\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2016-03-11 02:32:33 --> Unable to connect to the database
+INFO - 2016-03-11 02:32:33 --> Language file loaded: language/english/db_lang.php
+INFO - 2016-03-11 07:43:26 --> Config Class Initialized
+INFO - 2016-03-11 07:43:26 --> Hooks Class Initialized
+DEBUG - 2016-03-11 07:43:26 --> UTF-8 Support Enabled
+INFO - 2016-03-11 07:43:26 --> Utf8 Class Initialized
+INFO - 2016-03-11 07:43:26 --> URI Class Initialized
+INFO - 2016-03-11 07:43:26 --> Router Class Initialized
+INFO - 2016-03-11 07:43:26 --> Output Class Initialized
+INFO - 2016-03-11 07:43:26 --> Security Class Initialized
+DEBUG - 2016-03-11 07:43:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-03-11 07:43:26 --> Input Class Initialized
+INFO - 2016-03-11 07:43:26 --> Language Class Initialized
+INFO - 2016-03-11 07:43:26 --> Loader Class Initialized
+INFO - 2016-03-11 07:43:26 --> Helper loaded: url_helper
+INFO - 2016-03-11 07:43:26 --> Session: Class initialized using 'files' driver.
+INFO - 2016-03-11 07:43:26 --> Controller Class Initialized
+INFO - 2016-03-11 07:43:26 --> Model Class Initialized
+INFO - 2016-03-11 07:43:26 --> Database Driver Class Initialized
+INFO - 2016-03-11 07:43:26 --> Model Class Initialized
+INFO - 2016-03-11 07:43:26 --> table 
+INFO - 2016-03-11 07:43:26 --> File loaded: D:\soft\xampp\htdocs\cloud_system\application\views\login.html
+INFO - 2016-03-11 07:43:26 --> Final output sent to browser
+DEBUG - 2016-03-11 07:43:26 --> Total execution time: 0.3310
